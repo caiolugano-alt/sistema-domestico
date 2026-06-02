df140a6a99c3a8bb70e2b3641e04d6e18da58385
caiolugano-alt <caio.lugano@gmail.com>
2026-06-02 03:03:39 -0300

    Update relatorios.js

--- relatorios.js
+++ relatorios.js
@@ -2,28 +2,32 @@ async function carregarRelatorio() {
 
 try {
 
-const resposta = await fetch("/compras/relatorio")
-
-const dados = await resposta.json()
-
 const tabela = document.getElementById("tabelaRelatorio")
 
 tabela.innerHTML = ""
 
-dados.compras.forEach(compra => {
+let total = 0
+
+const snapshot = await db.collection("produtos").get()
+
+snapshot.forEach(doc => {
+
+const p = doc.data()
+
+total += Number(p.preco || 0)
 
 tabela.innerHTML += `
 <tr>
-<td>${compra.produto}</td>
-<td>${compra.categoria || "-"}</td>
-<td>${compra.quantidade}</td>
-<td>R$ ${Number(compra.preco).toFixed(2)}</td>
+<td>${p.nome}</td>
+<td>-</td>
+<td>${p.qtd}</td>
+<td>R$ ${Number(p.preco).toFixed(2)}</td>
 </tr>
 `
 })
 
 document.getElementById("totalGeral").innerHTML =
-`TOTAL GERAL: R$ ${dados.total.toFixed(2)}`
+"Total Geral: R$ " + total.toFixed(2)
 
 } catch (erro) {
 
@@ -31,9 +35,7 @@ console.error(erro)
 
 document.getElementById("totalGeral").innerHTML =
 "Erro ao carregar relatório"
-
 }
-
 }
 
 carregarRelatorio()
